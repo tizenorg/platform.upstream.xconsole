798ad4a31ca485e2b04a2a623b1fc96f3860d11f
Egbert Eich <eich@suse.de>
2004-02-26 13:36:16 +0000

    readding XFree86's cvs IDs

--- xconsole.c
+++ xconsole.c
@@ -26,7 +26,7 @@ in this Software without prior written authorization from The Open Group.
  * Author:  Keith Packard, MIT X Consortium
  */
 
-/* $XFree86$ */
+/* $XFree86: xc/programs/xconsole/xconsole.c,v 3.32 2003/05/27 22:26:59 tsi Exp $ */
 
 #include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
